25c3ef693fdc0a2afdbd1f74e8f87c9f36ee13e4
bcc5160 <bc.chiu12@gmail.com>
2016-03-27 21:31:55 -0400

    initial files (unformatted)

---
+++ jobs.js
@@ -0,0 +1,25 @@
+function getTimeRemaining(endtime){
+  var t = Date.parse(endtime) - Date.parse(new Date());
+  var seconds = Math.floor( (t/1000) % 60 );
+  var minutes = Math.floor( (t/1000/60) % 60 );
+  var hours = Math.floor( (t/(1000*60*60)) % 24 );
+  var days = Math.floor( t/(1000*60*60*24) );
+  return {
+    'total': t,
+    'days': days,
+    'hours': hours,
+    'minutes': minutes,
+    'seconds': seconds
+  };
+}
+
+// Time is passed in minutes
+function countDown(time){
+  
+  var t = time * 60;
+
+  for(i = t; i > 0; i--){
+    document.getElementById('time').innerHTML = "43:00";
+  }
+
+}
