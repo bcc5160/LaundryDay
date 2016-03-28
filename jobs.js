936caa37f6efbf135638128105e398b92604c658
bcc5160 <bc.chiu12@gmail.com>
2016-03-28 14:25:27 -0400

    changed title and getting changes

--- jobs.js
+++ jobs.js
@@ -17,9 +17,29 @@ function getTimeRemaining(endtime){
 function countDown(time){
   
   var t = time * 60;
-
+  console.log(t);
   for(i = t; i > 0; i--){
-    document.getElementById('time').innerHTML = "43:00";
+    setTimeout('', 1000);
+    document.getElementById('time').innerHTML = i.toString();
   }
 
+}
+
+function timer(time){
+var timeLabel = document.getElementById("time");
+var counter = time * 60;
+var newElement = document.createElement("p");
+var id;
+
+timeLabel.parentNode.replaceChild(newElement, timeLabel);
+
+id = setInterval(function() {
+    counter--;
+    if(counter < 0) {
+        newElement.parentNode.replaceChild(timeLabel, newElement);
+        clearInterval(id);
+    } else {
+        newElement.innerHTML = "Time remaining: " + counter.toString() + " seconds.";
+    }
+}, 1000);
 }
